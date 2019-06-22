69b6440b329ddba1e7de324d3242fed5187d349e
Marius Tomescu <marius.tomescu@mindmaze.ch>
2019-06-22 19:01:10 +0300

    Added camera and few post processing effects. Made relevant objects static to reduce the drawcalls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	private const float AxisSpeed = 3f;
+
+	private float _yaw = 0f;
+	private float _pitch = 0f;
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			var currentAngle = transform.rotation.eulerAngles;
+			currentAngle.y += 45f;
+			transform.rotation = Quaternion.Euler(currentAngle); // todo find a way to make it smoother (lerp)
+		}
+
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			var currentAngle = transform.rotation.eulerAngles;
+			currentAngle.y -= 45f;
+			transform.rotation = Quaternion.Euler(currentAngle); // todo find a way to make it smoother (lerp)
+
+		}
+	}
+
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private const float TurnSpeed = 20f;
+
+	[SerializeField] private Animator _animator;
+	[SerializeField] private Rigidbody _rigidbody;
+
+	private float _verticalAxis;
+	private float _horizontalAxis;
+	private Vector3 _movement;
+	private Quaternion _rotation;
+
+	private void FixedUpdate()
+	{
+		float _horizontalAxis = Input.GetAxis("Horizontal");
+		float _verticalAxis = Input.GetAxis("Vertical");
+
+		_movement.Set(_horizontalAxis, 0f, _verticalAxis);
+		_movement.Normalize();
+
+		bool hasHorizontalInput = !Mathf.Approximately(_horizontalAxis, 0f);
+		bool hasVerticalInput = !Mathf.Approximately(_verticalAxis, 0f);
+		bool isWalking = hasHorizontalInput || hasVerticalInput;
+		_animator.SetBool("IsWalking", isWalking);
+
+		Vector3 desiredForward = Vector3.RotateTowards(transform.forward, _movement, TurnSpeed * Time.deltaTime, 0f);
+		_rotation = Quaternion.LookRotation(desiredForward);
+	}
+
+	private void OnAnimatorMove()
+	{
+		_rigidbody.MovePosition(_rigidbody.position + _movement * _animator.deltaPosition.magnitude);
+		_rigidbody.MoveRotation(_rotation);
+	}
+
+
+}
