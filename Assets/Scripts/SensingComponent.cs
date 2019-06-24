ccada7e7cc1e9f77bbb41a14d08aa5c633656eff
Marius Tomescu <marius.tomescu@mindmaze.ch>
2019-06-24 18:53:43 +0300

    initial working for sphere sensing

---
+++ Assets/Scripts/SensingComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensingComponent : MonoBehaviour
+{
+	private const float SensingRadius = 20f;
+
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			RaycastHit hit;
+
+			Physics.SphereCast(transform.position, SensingRadius, transform.forward, out hit, 10);
+			{
+				Debug.Log(hit);
+			}
+
+			Physics.OverlapSphere(transform.position, SensingRadius);
+		}
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = new Color(1, 0, 0, .3f);
+		Gizmos.DrawSphere(transform.position, 1);
+	}
+
+}
