a725184fabeca0c4df59866dbb794103328a9b39
Marius Tomescu <marius.tomescu@mindmaze.ch>
2019-06-25 15:36:16 +0300

    added first interactable object

---
+++ Assets/Scripts/DoorInteractible.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteractible : MonoBehaviour, IInteractible
+{
+	private const string DoorAnimatorParameter = "OpenDoor";
+	[SerializeField] private Animator _animator;
+
+	private bool _isDoorOpen;
+
+	public void Interact()
+	{
+		Debug.Log("Interacted with a door");
+		_isDoorOpen = !_isDoorOpen;
+		_animator.SetBool(DoorAnimatorParameter, _isDoorOpen);
+	}
+}
--- Assets/Scripts/SensingComponent.cs
+++ Assets/Scripts/SensingComponent.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class SensingComponent : MonoBehaviour
 {
-	private const float SensingRadius = 20f;
+	private const float SensingRadius = 1f;
 
 
 	private void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.R))
 		{
-			RaycastHit hit;
-
-			Physics.SphereCast(transform.position, SensingRadius, transform.forward, out hit, 10);
+			var colliders = Physics.OverlapSphere(transform.position, SensingRadius);
+			foreach (var obj in colliders)
 			{
-				Debug.Log(hit);
+				GetInterfaces(out List<IInteractible> interactibles, obj.gameObject);
+				foreach (var interactible in interactibles)
+				{
+					interactible.Interact();
+				}
 			}
-
-			Physics.OverlapSphere(transform.position, SensingRadius);
 		}
 	}
 
@@ -28,4 +29,31 @@ public class SensingComponent : MonoBehaviour
 		Gizmos.DrawSphere(transform.position, 1);
 	}
 
+	private static bool ImplementsInteractible(GameObject obj)
+	{
+		MonoBehaviour[] list = obj.GetComponents<MonoBehaviour>();
+		foreach (var mb in list)
+		{
+			if (mb is IInteractible)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static void GetInterfaces<T>(out List<T> resultList, GameObject objectToSearch) where T : class
+	{
+		MonoBehaviour[] list = objectToSearch.GetComponents<MonoBehaviour>();
+		resultList = new List<T>();
+		foreach (MonoBehaviour mb in list)
+		{
+			if (mb is T)
+			{
+				//found one
+				resultList.Add((T)((System.Object)mb));
+			}
+		}
+	}
 }
